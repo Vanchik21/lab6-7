ce4ddc3a46eaa174f23011eac84c3f78f52eb4da
Zakharov Ivan <ipz221_zia@student.ztu.edu.ua>
2024-08-29 17:39:08 +0300

    add sql quary

--- lab6/lab6.sql
+++ lab6/lab6.sql
@@ -1,1 +1,12 @@
+CREATE DATABASE lab6;
+USE lab6;
+CREATE TABLE users (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       name VARCHAR(100) NOT NULL,
+                       email VARCHAR(100) NOT NULL UNIQUE,
+                       password VARCHAR(255) NOT NULL
+);
+INSERT INTO users (name, email, password) VALUES
+                                              ('John', 'john.doe@example.com', 'password123'),
+                                              ('Vanchik21', '', 'password456');
 
